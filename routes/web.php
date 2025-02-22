34a26c006376ef6ee6f5b19247858d210e562833
Eflorida1024 <florida_emanuel@plpasig.edu.ph>
2025-02-22 18:30:23 +0800

    Exercise #2 - Laravel Architectural Concepts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+use App\Services\UserService;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route ::get('/test-container', function (Request $request):mixed{
+    $input = $request->input('key');
+    return $input;
+});
+
+Route::get('/test-provider', function (UserService $userService):mixed{
+    return $userService->ListUsers();
+});
+
+Route::get('/test-user',[UserController::class,'index']);
+
+Route::get('/test-facade', function (UserService $userService):mixed{
+    return Response::json($userService->ListUsers());
 });
